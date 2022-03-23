3471b84c9c8a66c2c6443dbca7f83bf1e8f11f31
lomesh <badhelomesh82@gmail.com>
2022-03-23 11:56:05 +0530

    order-management

---
+++ src/components/product/Orders.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { useOrder } from "../../context/ordercontext";
+
+const Orders = () => {
+  const { orderstate } = useOrder();
+
+  return (
+    <div>
+      <div className="all__orders">
+        <h1>All orders</h1>
+        {orderstate.orders.map((item) => (
+          <div key={item.paymentId} className="order__main">
+            <div>
+              <h4 className="order__confirm">Order Confirmed</h4>
+              <h4>Payment ID: {item.paymentId}</h4>
+              <h4>Total Amount : ₹ {item.amount}</h4>
+              <h4>Delivery Address : </h4>
+              <h5> {item.delivery.name}</h5>
+              <h5>{item.delivery.email}</h5>
+              <h5>
+                {item.delivery.address}, {item.delivery.city}
+              </h5>
+              <h5>
+                {item.delivery.state}, {item.delivery.zipcode}
+              </h5>
+            </div>
+            <div>
+              {item.products.map((prod) => (
+                <div className="order__outer">
+                  <div className="order__image">
+                    <img src={prod.image} />
+                  </div>
+                  <div className="order__info">
+                    <h4>Name : {prod.name}</h4>
+                    <h4>Category : {prod.category}</h4>
+                    <h4>Quantity : {prod.quantity}</h4>
+                    <h4>Price : ₹ {prod.price}</h4>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Orders;
--- src/index.js
+++ src/index.js
@@ -2,14 +2,31 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 import { makeServer } from "./server";
+import { FilterProvider } from "./context/filtercontext";
+import { WishlistProvider } from "./context/wishlistcontext";
+import { CartProvider } from "./context/cartcontext";
+import { AuthProvider } from "./context/authcontext";
+import { OrderProvider } from "./context/ordercontext";
 
-// Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AuthProvider>
+        <OrderProvider>
+          <CartProvider>
+            <WishlistProvider>
+              <FilterProvider>
+                <App />
+              </FilterProvider>
+            </WishlistProvider>
+          </CartProvider>
+        </OrderProvider>
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
